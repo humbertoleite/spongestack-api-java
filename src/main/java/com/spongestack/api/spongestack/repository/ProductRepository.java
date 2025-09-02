494e0e3c865538e91ea1864ad20b69b491f8fc89
Humberto <humberto@onecommerce.es>
2025-09-02 17:39:06 +0200

    Add ProductRepository interface for managing Product entities with methods to find by name and price

---
+++ src/main/java/com/spongestack/api/spongestack/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.spongestack.api.spongestack.repository;
+
+import com.spongestack.api.spongestack.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    
+    Optional<Product> findByName(String name);
+    
+    Optional<Product> findByPrice(Double price);
+}
